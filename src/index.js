9ee8cc40d3acb07745cae28eb1f1764e97054023
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-11-14 10:38:42 +0100

    added tests configuration and fonts

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+import '../node_modules/normalize.css/normalize.css';
+import './index.css';
+
 render(<h1>myRecipes</h1>, document.getElementById('root'));
 registerServiceWorker();
