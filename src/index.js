eae1a04a9c30d3d5a3982dc0aaf9b41c6e227036
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-11-14 10:58:04 +0100

    created redux store with sample resipes as a preloadedState

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
 import '../node_modules/normalize.css/normalize.css';
 import './index.css';
 
-render(<h1>myRecipes</h1>, document.getElementById('root'));
+import recipes from './recipes';
+
+import App from './components/App/App';
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  state => state,
+  recipes,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
